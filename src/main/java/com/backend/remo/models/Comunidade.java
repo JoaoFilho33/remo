792664a9bff60bd441768b34b2f5c58683815629
kairon <kairon@kairon.kairon>
2023-10-29 00:12:37 -0300

    b

--- src/main/java/com/backend/remo/models/Comunidade.java
+++ src/main/java/com/backend/remo/models/Comunidade.java
@@ -1,36 +1,20 @@
 package com.backend.remo.models;
 
-import com.fasterxml.jackson.annotation.JsonIgnore;
-import jakarta.persistence.*;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import java.sql.Timestamp;
-import java.util.List;
-
-@Entity
-@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Data
+@NoArgsConstructor @AllArgsConstructor @Builder @Entity
 public class Comunidade {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(name = "nome")
-    private String nome;
 
-    @Column(name = "descricao")
-    private String descricao;
-
-    @Column(name = "data_criacao")
-    private Timestamp dataCriacao;
-
-    @ManyToOne
-    @JoinColumn(name = "id_usuario_criador")
-    private Usuario usuario;
-
-    @JsonIgnore
-    @OneToMany(mappedBy = "comunidade")
-    private List<Participante> participantes;
 }
--- src/main/java/com/backend/remo/models/Participante.java
+++ src/main/java/com/backend/remo/models/Participante.java
@@ -1,6 +1,9 @@
 package com.backend.remo.models;
 
+<<<<<<< HEAD
 import com.fasterxml.jackson.annotation.JsonIgnore;
+=======
+>>>>>>> f394832 (b)
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -10,7 +13,10 @@ import lombok.NoArgsConstructor;
 import java.sql.Timestamp;
 import java.util.List;
 
+<<<<<<< HEAD
 //Participante é o relacionamento entre usuário e comunidade
+=======
+>>>>>>> f394832 (b)
 @Entity
 @Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class Participante {
@@ -32,7 +38,10 @@ public class Participante {
     @JoinColumn(name = "id_comunidade")
     private Comunidade comunidade;
 
+<<<<<<< HEAD
     @JsonIgnore
+=======
+>>>>>>> f394832 (b)
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "participante")
     private List<Wiki> wikis;
 }
