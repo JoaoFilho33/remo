5e20549a3adc1888a709d10090cd91e4c0fef4c1
Kairon Willyan <willyankairon1@gmail.com>
2023-11-05 17:18:46 -0300

    funcionando tudo

--- src/main/java/com/backend/remo/models/Segue.java
+++ src/main/java/com/backend/remo/models/Segue.java
@@ -6,19 +6,19 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-//@Entity
-//@Data @NoArgsConstructor @AllArgsConstructor @Builder
+@Entity
+@Data @NoArgsConstructor @AllArgsConstructor @Builder
 public class Segue {
-//    @Id
-//    @GeneratedValue(strategy = GenerationType.IDENTITY)
-//    private Long id;
-//
-//    @ManyToOne
-//    @JoinColumn(name = "seguidor_id")
-//    private Usuario seguidor; // Usuário que está seguindo
-//
-//    @ManyToOne
-//    @JoinColumn(name = "seguido_id")
-//    private Usuario seguido;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "seguidor_id")
+    private Usuario seguidor; // Usuário que está seguindo
+
+    @ManyToOne
+    @JoinColumn(name = "seguido_id")
+    private Usuario seguido; // Usuário que está sendo seguindo
 
 }
