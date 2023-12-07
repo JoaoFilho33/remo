42ccbab851b5c4a45d359cb17286072c050315f0
Kairon Willyan <willyankairon1@gmail.com>
2023-12-07 02:05:34 -0300

    pesq

--- ProjRemo/src/pages/Pesquisar/index.tsx
+++ ProjRemo/src/pages/Pesquisar/index.tsx
@@ -1,7 +1,49 @@
-export function Pesquisar(){
-    return(
-        <>
-        <h1>Pesquisar</h1>
-        </>
-    )
+import fetch from "node-fetch";
+import { useState, ChangeEvent } from "react";
+
+interface Collection {
+  id: number;
+  name: string;
+}
+
+export function Pesquisar() {
+  const [query, setQuery] = useState<string>("");
+  const [results, setResults] = useState<Collection[]>([]);
+
+  const handleSearch = async () => {
+    const url = `https://api.themoviedb.org/3/search/collection?query=${query}&include_adult=true&language=en-US&page=1`;
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        Authorization: "Bearer YOUR_API_KEY",
+      },
+    };
+
+    try {
+      const response = await fetch(url, options);
+      const json = await response.json();
+      setResults(json.results);
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  };
+  return (
+    <div>
+      <input
+        type="text"
+        value={query}
+        onChange={(e: ChangeEvent<HTMLInputElement>) =>
+          setQuery(e.target.value)
+        }
+      />
+      <button onClick={handleSearch}>Search</button>
+
+      <ul>
+        {results.map((result) => (
+          <li key={result.id}>{result.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
 }
